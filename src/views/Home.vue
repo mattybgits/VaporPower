1386ebfb6b1ac6db4ba4f57e26254f877cfd17d2
SoIidarity <christopher.maree@gmail.com>
2018-11-04 00:17:25 +0100

    Added tropy instead of donation icon:

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1>Welcome to EthStarter!</h1>
     <br>
-    <img alt="Vue logo" src="../assets/donate-solid.svg" style="width:250px">
+    <img alt="Vue logo" src="../assets/trophy-solid.svg" style="width:250px">
     <HelloWorld class="center"/>
   </div>
 </template>
